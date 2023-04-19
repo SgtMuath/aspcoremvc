0568061a75621590e5c92fae15198f79047dc4ae
omar <omar@DESKTOP-KP1452F>
2023-04-19 07:12:38 +0300

    Add project files.

---
+++ aspcoremvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspcoremvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
